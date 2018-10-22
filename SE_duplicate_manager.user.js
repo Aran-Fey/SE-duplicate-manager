5e1a8639147b079eafb03247be7d6bc9d58f42fd
aran-fey <rawing7@gmail.com>
2018-10-22 17:47:01 +0200

    updated libraries

--- SE_duplicate_manager.user.js
+++ SE_duplicate_manager.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         StackExchange duplicate manager
 // @description  Lets you mark questions as commonly used duplicate targets, and search through your collection of duplicate targets from within the close question dialog
-// @version      1.2.3
+// @version      1.2.4
 // @author       Paul Pinterits
 // @include      *://*.stackexchange.com/questions/*
 // @include      *://meta.serverfault.com/questions/*
@@ -20,7 +20,7 @@
 // @exclude      *://*/questions/tagged/*
 // @exclude      *://*/questions/ask
 // @namespace    Aran-Fey
-// @require      https://github.com/Aran-Fey/userscript-lib/raw/32d65b6a5c357414fe2d0c74645f6af1fe0e88db/userscript_lib.js
+// @require      https://github.com/Aran-Fey/userscript-lib/raw/0b47391750e823901a57f0264470b2bef4dc602c/userscript_lib.js
 // @require      https://github.com/Aran-Fey/SE-userscript-lib/raw/80f15404a4f5d620ae68ebd67876daa1dadfb464/SE_userscript_lib.js
 // @grant        GM_setValue
 // @grant        GM_getValue

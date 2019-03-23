d4ad78b2a9afbc1e158887510dbab546f50684f0
Aran-Fey <rawing7@gmail.com>
2019-03-23 22:13:09 +0100

    search function now also searches titles

--- SE_duplicate_manager.user.js
+++ SE_duplicate_manager.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         StackExchange duplicate manager
 // @description  Lets you mark questions as commonly used duplicate targets, and search through your collection of duplicate targets from within the close question dialog
-// @version      1.2.6
+// @version      1.2.8
 // @author       Paul Pinterits
 // @include      *://*.stackexchange.com/questions/*
 // @include      *://meta.serverfault.com/questions/*
@@ -91,19 +91,19 @@ function save_originals(){
  * Creates the button that adds/removes a question to/from the collection.
  */
 function make_collection_toggle_button(){
-    const fav_button = document.querySelector('.star-off');
+    const fav_button = document.querySelector('.js-favorite-btn');
     if (fav_button === null)
         return;
     
-    const toggle_button = document.createElement('A');
+    const toggle_button = document.createElement('BUTTON');
     toggle_button.id = 'toggle-original-button';
     toggle_button.onclick = toggle_in_collecton;
-    toggle_button.classList.add('is-original-off');
+    toggle_button.classList.add('is-original-off', 's-btn', 's-btn__unset');
     
     const icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     icon.innerHTML = icon_code;
     icon.setAttribute('viewBox', '0 0 50 50');
-    icon.style.width = icon.style.height = '25px';
+    icon.style.width = icon.style.height = '22px';
     toggle_button.appendChild(icon);
     
     const parent = fav_button.parentElement;
@@ -448,6 +448,10 @@ async function insert_suggestions(clear_other_suggestions){
                 if (kword.includes(term))
                     score += term.length * 5;
             }
+
+            // check if the question title contains this search term
+            if (original.title.includes(term))
+                score += term.length * 3;
             
             // check if the question text contains this search term
             if (original.text.includes(term))
